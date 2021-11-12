f507eb92528d9dcbc36a1a0b57cb8255f751b184
GhastCMD <kraken32bin@protonmail.com>
2021-11-12 18:49:36 -0300

    Implemented list training

--- exercises/0 find h or m in matrix/Python/DualPerceptronNetwork.py
+++ exercises/0 find h or m in matrix/Python/DualPerceptronNetwork.py
@@ -1,35 +1,44 @@
 import numpy as np
 
+np.random.seed(1)
+
 class DualPerceptronNetwork:
-    bias = 1
-    bias_weight = 0.5
+    bias = 0.5
     
     weights0 = np.zeros(25)
     weights1 = np.zeros(25)
     
     learning_rate = 1
     
+    def train_list(self, questions: np.array, answers: np.array, epochs: int):
+        choice = np.arange(len(questions))
+        np.random.shuffle(choice)
+        
+        for _ in range(epochs):
+            for i in choice:
+                question = questions[i].flatten()
+                answer = answers[i]
+                self.train(question, answer)
+    
     def train(self, question: np.array, right_answer: np.array):
         error_rate = np.ones(2)
+        infered_answer = self.infer(question)
         
-        while error_rate.any() != 0:
-            infered_answer = self.infer(question)
-            
-            error_rate[0] = right_answer[0] - infered_answer[0]
-            error_rate[1] = right_answer[1] - infered_answer[1]
+        error_rate = right_answer - infered_answer
 
-            self.weights0 += self.learning_rate * error_rate[0] * question
-            self.weights1 += self.learning_rate * error_rate[1] * question
+        self.weights0 += self.learning_rate * error_rate[0] * question
+        self.weights1 += self.learning_rate * error_rate[1] * question
         
     
     def infer(self, question: np.array):
-        y = np.array([0,0])
+        y = np.zeros(2)
+        question = question.flatten()
         
         y[0] = np.sum(self.weights0 * question)
         y[1] = np.sum(self.weights1 * question)
         
-        y[0] += self.bias * self.bias_weight
-        y[1] += self.bias * self.bias_weight
+        for value in y:
+            value += self.bias
         
         ret_array = np.array(y >= 0, dtype=int)
         return ret_array
