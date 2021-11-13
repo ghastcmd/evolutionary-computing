31ee762447074b7376109e4e3303553397e2c9d3
GhastCMD <kraken32bin@protonmail.com>
2021-11-13 10:13:40 -0300

    Added some test example

--- exercises/1 recognize shapes/python/main.py
+++ exercises/1 recognize shapes/python/main.py
@@ -1,2 +1,33 @@
+import numpy as np
+from PerceptronNetwork import PerceptronNetwork
+
 if __name__ == '__main__':
-    print('hello world')
+    network = PerceptronNetwork(25, 2)
+    
+    h = np.array([
+        1, 0, 0, 0, 1,
+        1, 0, 0, 0, 1,
+        1, 1, 1, 1, 1,
+        1, 0, 0, 0, 1,
+        1, 0, 0, 0, 1
+    ])
+    h_answer = [1,0]
+    
+    m = np.array([
+        1, 0, 0, 0, 1,
+        1, 1, 0, 1, 1,
+        1, 0, 1, 0, 1,
+        1, 0, 0, 0, 1,
+        1, 0, 0, 0, 1
+    ])
+    m_answer = [0,1]
+    
+    null = np.zeros(25)
+    null_answer = [1,1]
+    
+    questions = np.vstack((h, m, null))
+    answers = np.vstack((h_answer, m_answer, null_answer))
+    
+    network.train_list(questions, answers, 20)
+    
+    print(network.infer(m))
