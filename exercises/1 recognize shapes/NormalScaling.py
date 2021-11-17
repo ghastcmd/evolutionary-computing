d0c0c379aa913b0a880cac81e0bfd0d142354250
GhastCMD <kraken32bin@protonmail.com>
2021-11-17 18:51:07 -0300

    Added another test

--- exercises/1 recognize shapes/NormalScaling.py
+++ exercises/1 recognize shapes/NormalScaling.py
@@ -4,24 +4,25 @@ def scale(matrix: np.ndarray, final_shape: tuple):
     ret_matrix = np.zeros(final_shape, dtype=int)
     height, width = final_shape
     orig_height, orig_width = matrix.shape
-    scale_width = int(width/orig_width)
-    scale_height = int(height/orig_height)
-     
+    scale_width = width/orig_width
+    scale_height = height/orig_height
+    
     for y in range(int(height/scale_height)):
         yy = int(y * scale_height)
         for x in range(int(width/scale_width)):
             xx = int(x * scale_width)
-            ret_matrix[yy:yy+scale_height,xx:xx+scale_width] = matrix[y, x]
+            ret_matrix[yy:yy+int(scale_height),xx:xx+int(scale_width)] = matrix[y, x]
 
     return ret_matrix
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
     class Test:
         def __init__(self):
             self.num_test = 1
 
         def test_print(self, mat1, mat2):
             print(f'Test {self.num_test:02}: ', (mat1 == mat2).all())
+            # print(mat1)
             self.num_test += 1
 
         def test(self, little_matrix, final_matrix):
@@ -59,3 +60,12 @@ if __name__ == '__main__':
     test_matrix[10:20,0:10] = 1
     
     tt.test(little_matrix, test_matrix)
+    
+    little_matrix = np.zeros((19, 19))
+    little_matrix[0:,0] = 1
+    
+    final_matrix = np.zeros((20,20))
+    final_matrix[:19,0] = 1
+    
+    tt.test(little_matrix, final_matrix)
+    
