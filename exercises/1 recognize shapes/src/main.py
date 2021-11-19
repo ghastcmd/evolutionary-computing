80e5a97f4b2e0c67c4095f5e2e2ffa0dbe311539
GhastCMD <kraken32bin@protonmail.com>
2021-11-19 15:57:23 -0300

    Implemented the scan window function

--- exercises/1 recognize shapes/src/main.py
+++ exercises/1 recognize shapes/src/main.py
@@ -1,5 +1,32 @@
 import numpy as np
+import os
 from PerceptronNetwork import PerceptronNetwork
+from NormalScaling import scale
+
+def scan_window(matrix, neural_network: PerceptronNetwork):
+    num_shapes = 0
+    for window in range(2, 21):
+        for x in range(1, 20 - window):
+            for y in range(1, 20 - window):
+                view = matrix[y:y+window,x:x+window]
+                scaled_matrix = scale(view, (20,20))
+                num_shapes += neural_network.infer(scaled_matrix)
+
+    return num_shapes
+
+if __name__ == '__main__':
+    network = PerceptronNetwork(20*20, 1)
+    
+    if os.path.exists('shapes_network.data'):
+        network.load('shapes_network.data')
+    else:
+        print('You need to train the model first')
+        exit()    
+
+
+exit()
+
+# testing the neural network
 
 if __name__ == '__main__':
     network = PerceptronNetwork(25, 2)
