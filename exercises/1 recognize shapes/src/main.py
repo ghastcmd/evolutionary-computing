05330738742e008b9b513317903d577cb0056829
GhastCMD <kraken32bin@protonmail.com>
2021-11-19 16:19:07 -0300

    Implemented the reading of the training file

--- exercises/1 recognize shapes/src/main.py
+++ exercises/1 recognize shapes/src/main.py
@@ -10,7 +10,8 @@ def scan_window(matrix, neural_network: PerceptronNetwork):
             for y in range(1, 20 - window):
                 view = matrix[y:y+window,x:x+window]
                 scaled_matrix = scale(view, (20,20))
-                num_shapes += neural_network.infer(scaled_matrix)
+                prediction = neural_network.infer(scaled_matrix)
+                num_shapes += prediction
 
     return num_shapes
 
@@ -19,38 +20,48 @@ def read_training_file(path):
     ret_answers = []
     
     with open(path, 'r') as fp:
-        mat_shape = fp.readline().strip().split(' ')
-        mat_shape = tuple([int(x) for x in mat_shape])
-        mat_list = []
-        for _ in range(mat_shape[0]):
-            line_mat = fp.readline().strip().split(' ')
-            line_mat = [int(x) for x in line_mat]
-            mat_list.append(line_mat)
-        
-        mat_list = np.array(mat_list, dtype=int).reshape(mat_shape)
-        mat_list = scale(mat_list, (20,20))
-        
-        ret_matrix.append(mat_list)
-        
-        ret_answers.append(int(fp.readline().strip()))
-        fp.readline()
+        while True:
+            start_read = fp.readline()
+            if start_read == '':
+                break
+            elif start_read == '\n':
+                start_read = fp.readline()
+            
+            mat_shape = start_read.strip().split(' ')
+            mat_shape = tuple([int(x) for x in mat_shape])
+            mat_list = []
+            for _ in range(mat_shape[0]):
+                line_mat = fp.readline().strip().split(' ')
+                line_mat = [int(x) for x in line_mat]
+                mat_list.append(line_mat)
+            
+            mat_list = np.array(mat_list, dtype=int).reshape(mat_shape)
+            mat_list = scale(mat_list, (20,20))
+            
+            ret_matrix.append(mat_list)
+            
+            ret_answers.append(int(fp.readline().strip()))
     
-    return ret_matrix, ret_answers
+    return np.array(ret_matrix), np.array(ret_answers)
 
 if __name__ == '__main__':
+    network = PerceptronNetwork(20*20, 1)
     
-    values, answers = read_training_file('training.dat')
+    if os.path.exists('shapes_network.data'):
+        network.load('shapes_network.data')
+    else:
+        if os.path.exists('training.vv'):
+            values = read_training_file('training.vv')
+            questions, answers = values
+        else:
+            print('Need to have a training file')
+            exit()
+        network.train_list(questions, answers, 10)
     
-    print(values, '\n', answers)
+    input_matrix = np.zeros((20,20))
     
-    
-    # network = PerceptronNetwork(20*20, 1)
-    
-    # if os.path.exists('shapes_network.data'):
-    #     network.load('shapes_network.data')
-    # else:
-    #     print('You need to train the model first')
-    #     exit()    
+    num_shapes = scan_window(input_matrix, network)
+    print(f'There is {num_shapes} horizontal rectangles in the matrix')
 
 
 exit()
