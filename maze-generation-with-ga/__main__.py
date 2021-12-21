8c0bf84a22c61a05cd33b8ada323f3e4ba95e042
GhastCMD <kraken32bin@protonmail.com>
2021-12-21 17:57:11 -0300

    Added a function to generate graph from maze

--- maze-generation-with-ga/__main__.py
+++ maze-generation-with-ga/__main__.py
@@ -24,17 +24,49 @@ class Maze:
     def print_maze(self):
         for line in self.maze:
             print(line)
+    
+    def generate_graph(self):
+        graph = []
+        
+        for y, line in enumerate(self.maze):
+            for x in range(len(line)):
+                up = (y - 1) * self.width + x
+                right = y * self.width + x + 1
+                left = y * self.width + x - 1
+                down = (y + 1) * self.width + x
+                
+                append_value = []
+                              
+                if y != 0:
+                    append_value.append(up)
+                
+                if x != 0:
+                    append_value.append(left)
+                
+                if y != self.height-1:
+                    append_value.append(down)
+                    
+                if x != self.width-1:
+                    append_value.append(right)
+                
+                graph.append(append_value)
+        
+        return graph
 
-default_maze = Maze([
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0]
-])
+if __name__ == '__main__':
 
-print(default_maze.width)
-default_maze.color_points()
-default_maze.print_maze()
+    test_maze = Maze([
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ])
+
+    assert test_maze.width == 3
+    assert test_maze.height == 3
+
+    test_maze.color_points()
+    assert test_maze.maze == [[-2, 0, 0], [0, 0, -1], [0, 0, 0]]
+
+    graph = test_maze.generate_graph()
+
+    assert graph == [[3, 1], [0, 4, 2], [1, 5], [0, 6, 4], [1, 3, 7, 5], [2, 4, 8], [3, 7], [4, 6, 8], [5, 7]]
