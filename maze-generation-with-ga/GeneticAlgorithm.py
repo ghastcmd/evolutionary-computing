f0af67f841b5c197be9923d7f18426a46e438e80
GhastCMD <kraken32bin@protonmail.com>
2021-12-22 13:28:03 -0300

    Added the crossover function

--- maze-generation-with-ga/GeneticAlgorithm.py
+++ maze-generation-with-ga/GeneticAlgorithm.py
@@ -1,4 +1,5 @@
 from Maze import Maze
+import random
 
 def fitness(maze: Maze):
     fit_value = 0
@@ -15,3 +16,27 @@ def fitness(maze: Maze):
                 fit_value += 1
     
     return fit_value
+
+def crossover(maze1: Maze, maze2: Maze):
+    assert maze1.height == maze2.height
+    assert maze1.width == maze2.width
+    
+    max_len = maze1.width * maze1.height
+    point = random.choice(range(max_len))
+    
+    ret_maze1 = maze1.maze[:point] + maze2.maze[point:]
+    ret_maze1 = Maze(ret_maze1)
+    
+    ret_maze2 = maze2.maze[:point] + maze1.maze[point:]
+    ret_maze2 = Maze(ret_maze2)
+    
+    return (ret_maze1, ret_maze2)
+
+
+if __name__ == '__main__':
+    maze1 = Maze([[1,1], [1,1]])
+    maze2 = Maze([[2,2], [2,2]])
+    
+    aaa1, aaa2 = crossover(maze1, maze2)
+    print(aaa1)
+    print(aaa2)
