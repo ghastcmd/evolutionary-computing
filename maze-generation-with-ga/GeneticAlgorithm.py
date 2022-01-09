ed9cc668a8daf1754544fc50f84a48c965bd9877
GhastCMD <kraken32bin@protonmail.com>
2022-01-09 12:58:17 -0300

    Corrected bug which best maze was not in new generation

--- maze-generation-with-ga/GeneticAlgorithm.py
+++ maze-generation-with-ga/GeneticAlgorithm.py
@@ -8,7 +8,7 @@ def fitness(maze: Maze) -> int:
     if max_path == 0:
         fit_value -= maze.width * maze.height
         
-    fit_value += max_path * 2
+    fit_value += max_path
     
     for line in maze.maze:
         for value in line:
@@ -21,8 +21,8 @@ def crossover(maze1: Maze, maze2: Maze) -> tuple[Maze, Maze]:
     assert maze1.height == maze2.height
     assert maze1.width == maze2.width
     
-    max_len = maze1.width * maze1.height
-    point = random.choice(range(max_len))
+    # max_len = maze1.width * maze1.height
+    point = random.choice(range(maze1.height))
     
     ret_maze1 = maze1.maze[:point] + maze2.maze[point:]
     ret_maze1 = Maze(ret_maze1)
@@ -36,7 +36,7 @@ def mutate(maze: Maze, prob=0.3):
     for y in range(maze.height):
         for x in range(maze.width):
             if random.uniform(0, 1) <= prob:
-                val = maze.maze[y][y]
+                val = maze.maze[y][x]
                 if val == 0 or val == 1:
                     maze.maze[y][x] = int(not bool(val))
 
@@ -62,15 +62,55 @@ def gen_init_pop(start_maze: Maze, num_pop: int, mutate_rate: float = 0.3):
     
     return start_population
 
-def run(num_population: int, num_elite: int):
-    start_maze = Maze(gen_matrix(4, 4))
-    initial_pop = gen_init_pop(start_maze, num_population)
+def run(
+    num_population: int, num_elite: int, generations: int,
+    shape: tuple[int, int] = (4,4)
+):
+    start_maze = Maze(gen_matrix(*shape))
+    initial_pop = gen_init_pop(start_maze, num_population, 0.5)
     
-    for maze in initial_pop:
-        print(maze, '\n')
+    best_ret_mazes = []
+    
+    for _ in range(generations):
+        population = initial_pop
+        
+        scored_mazes = []
+        for maze in population:
+            score = fitness(maze)
+            scored_mazes.append((score, maze))
+        
+        scored_mazes.sort(key=lambda x: x[0])
+        scored_mazes.reverse()
+        
+        print('#### The best maze was ####')
+        print(f'Score: {scored_mazes[0][0]},\n{scored_mazes[0][1]}')
+        print('############\n')
+        best_ret_mazes.append(scored_mazes[0][1])
+
+        best_mazes = scored_mazes[:num_elite]
+
+        new_generation = []
+        for values in best_mazes:
+            new_generation.append(values[1].copy())
+        
+        for _ in range(num_population//2 - num_elite):
+            first_maze = random.choice(best_mazes)[1].copy()
+            second_maze = random.choice(best_mazes)[1].copy()
+
+            ret_maze = crossover(first_maze, second_maze)
+            
+            for maze in ret_maze:
+                mutate(maze, 0.2)
+            
+            new_generation.extend(ret_maze)
+
+        population = new_generation
+
+    return best_ret_mazes
+
 
 if __name__ == '__main__':
-    run(100, 10)
+    run(100, 10, 3)
     
     maze1 = Maze([[1,1], [1,1]])
     maze2 = Maze([[0,0], [0,0]])
