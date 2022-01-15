cc1803f3dc4812adf44b72a6ffc39c2a576e1345
GhastCMD <kraken32bin@protonmail.com>
2022-01-15 16:37:11 -0300

    Added better get_mazes function

--- maze-generation-with-ga/__main__.py
+++ maze-generation-with-ga/__main__.py
@@ -1,7 +1,11 @@
 from GeneticAlgorithm import run
 
+def get_levels(quantity: int = 2, step: int = 2):
+   max_gen = quantity * step
+   return run(100, 10, max_gen, 0.1, shape=(12, 48), quantity=quantity)
+
 if __name__ == '__main__':
-   mazes = run(100, 10, 10, 0.1, shape=(12, 48), quantity=2)
+   mazes = get_levels(2)
       
    print('best mazes are:')
    print(f'The len of mazes: {len(mazes)}')
