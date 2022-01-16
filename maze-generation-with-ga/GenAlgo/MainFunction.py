8a5487f155e887da09082d52aad2b239aa7607f6
GhastCMD <kraken32bin@protonmail.com>
2022-01-16 16:13:35 -0300

    Added two files

---
+++ maze-generation-with-ga/GenAlgo/MainFunction.py
@@ -0,0 +1,21 @@
+from GeneticAlgorithm import run
+
+def get_levels(quantity: int = 2, step: int = 2):
+   max_gen = quantity * step
+   return run(100, 10, max_gen, 0.1, shape=(12, 48), quantity=quantity)
+
+if __name__ == '__main__':
+   mazes = get_levels(2)
+   
+   for maze in mazes:
+      for line in maze:
+         print(line)
+      print("")
+   
+   # for maze in mazes:
+   #    print(maze, '\n')
+      
+   print('best mazes are:')
+   print(f'The len of mazes: {len(mazes)}')
+   # for maze in mazes:
+   #    print(maze, '\n')
---
+++ maze-generation-with-ga/__main__.py
@@ -0,0 +1,7 @@
+# from medieval_maze.singlePlayer import main
+
+from src.MainFunction import get_levels
+
+get_levels(2)
+
+# main()
